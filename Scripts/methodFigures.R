b951f811d6ea35de3593569a425ffb9355185429
Fadel Megahed <fmegahed@miamioh.edu>
2021-05-06 10:58:06 -0400

    Finalizing plots for second revision

--- Scripts/methodFigures.R
+++ Scripts/methodFigures.R
@@ -1,10 +1,10 @@
-# scripts to create Some Covid19 visualizations
+### scripts to create Some Covid19 visualizations
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # automatically set working dir
 
-pacman::p_load(tidyverse, COVID19, magrittr, lubridate, fpp2, zoo, tmap, albersusa, tigris, RColorBrewer) # loading required packages
+pacman::p_load(tidyverse, COVID19, magrittr, lubridate, fpp2, zoo, tmap, albersusa, tigris, RColorBrewer, ggpubr) # loading required packages
 
-# National Data
+### National Data ----
 us = covid19(country = 'US', level = 1, start = '2020-03-01', end = '2021-01-02')
 us %<>% select(id, key_google_mobility, date, confirmed) %>% 
   mutate(newCases = c(NA, diff(confirmed)),
@@ -12,7 +12,40 @@ us %<>% select(id, key_google_mobility, date, confirmed) %>%
          maxMM7 = max(newMM7, na.rm = T), # obtaining the max per county to scale data
          scaledNewMM7 = pmax(0, newMM7/maxMM7, na.rm = TRUE))
 
-# Counties Data
+### * US Plot: usPlot ----
+tiff(filename = '../Figures/usPlot.tiff',
+     width = 1920, height =1080, pointsize = 16)
+us %>% ggplot(aes(x = date, y = newCases)) +
+  geom_line(aes(color = "New Cases")) +
+  geom_line(aes(y = newMM7, color = "7-Day Moving Median of New Cases"), size = 3 ) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw(base_size = 24) + 
+  theme(legend.position = 'top') + 
+  scale_colour_manual("Lines", values=c("New Cases"="black", "7-Day Moving Median of New Cases"="red")) +
+  labs(color = '', x = 'Month', y = 'New Cases Aggregated for the Entire US',
+       caption = 'Based on data from 2020-03-01 to 2021-01-02')
+invisible( dev.off() ) # to suppress the unwanted output from dev.off
+
+
+### * US Plot: EPS usPlot ----
+postscript(file = '../Figures/usPlot.eps',
+     width = 1920, height =1080, pointsize = 16)
+us %>% ggplot(aes(x = date, y = newCases)) +
+  geom_line(aes(color = "New Cases")) +
+  geom_line(aes(y = newMM7, color = "7-Day Moving Median of New Cases"), size = 3 ) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw(base_size = 24) + 
+  theme(legend.position = 'top') + 
+  scale_colour_manual("Lines", values=c("New Cases"="black", "7-Day Moving Median of New Cases"="red")) +
+  labs(color = '', x = 'Month', y = 'New Cases Aggregated for the Entire US',
+       caption = 'Based on data from 2020-03-01 to 2021-01-02')
+invisible( dev.off() ) # to suppress the unwanted output from dev.off
+
+
+
+### Counties Data ----
 counties = covid19(country = 'US', level = 3, start = '2020-03-01', end = '2021-01-02',
                    raw = FALSE)
 
@@ -22,43 +55,51 @@ counties %<>% select(id, key_google_mobility, date, confirmed) %>%
          maxMM7 = max(newMM7, na.rm = T), # obtaining the max per county to scale data
          scaledNewMM7 = pmax(0, newMM7/maxMM7, na.rm = TRUE))
 
-indices = c('Alabama, Lee County', 'Arizona, Navajo County', 'California, San Francisco County',
+indices = c('Alabama, Lee County', 'Arizona, Navajo County', 'Arkansas, Washington County', 'California, San Francisco County',
             'Illinois, Madison County', 'New York, New York County','Ohio, Butler County',
             'Virginia, Charlottesville', 'Virginia, Falls Church') # some counties
 
 # filtering some counties and combining both data frames
 df = counties %>% filter(key_google_mobility %in% indices)
-df = rbind(us, df)
-
-df$key_google_mobility %<>% recode(US = 'Aggregate for the Entire US') 
 
 
-# Facet Wrap Plot: nonScaledMotivationPlot
-png(filename = '../Figures/nonScaledMotivationPlot.png',
-     width = 1920, height =1080, pointsize = 16)
+
+# * Motivation Plot: Parts A and B ----
+
 df %>% ggplot(aes(x = date, y = newMM7, group = id)) +
   geom_line(size = 1.5) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b") +
   facet_wrap(~ key_google_mobility, scales = 'free_y', ncol = 3) + 
-  theme_bw(base_size = 24) + 
+  theme_bw(base_size = 30) + 
   theme(legend.position = 'none') + 
   labs(color = '', x = 'Month', y = 'New Cases By County',
-       caption = 'Based on data from 2020-03-01 to 2021-01-02')
-invisible( dev.off() ) # to suppress the unwanted output from dev.off
+       caption = 'Based on data from 2020-03-01 to 2021-01-02') -> p1
 
 
-# Facet Wrap Plot: motivationPlot
-png(filename = '../Figures/motivationPlot.png',
-     width = 1920, height = 1080, pointsize = 16)
 df %>% ggplot(aes(x = date, y = scaledNewMM7, group = id)) +
   geom_line(size = 1.5) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b") +
   facet_wrap(~ key_google_mobility, scales = 'free_y', ncol = 3) + 
-  theme_bw(base_size = 24) + 
-  theme(legend.position = 'none') +
-  labs(color = '', x = 'Month', y = 'New Cases By County',
-       caption = 'Based on data from 2020-03-01 to 2021-01-02')
+  theme_bw(base_size = 30) + 
+  theme(legend.position = 'none') + 
+  labs(color = '', x = 'Month', y = 'Scaled New Cases By County',
+       caption = 'Based on data from 2020-03-01 to 2021-01-02') -> p2
+
+tiff(filename = '../Figures/motivationPlot.tiff',
+     width = 1800, height =1800, pointsize = 28)
+ggpubr::ggarrange(p1, p2, ncol = 1, labels = c('(a)', '(b)'),
+                  font.label =  list(size = 45, face = "bold", color ="black"))
 invisible( dev.off() ) # to suppress the unwanted output from dev.off
+
+
+
+png(filename = '../Figures/motivationPlot.png',
+           width = 1800, height =1800, pointsize = 28)
+ggpubr::ggarrange(p1, p2, ncol = 1, labels = c('(a)', '(b)'),
+                  font.label =  list(size = 45, face = "bold", color ="black"))
+invisible( dev.off() ) # to suppress the unwanted output from dev.off
+
+
 
 
 
@@ -145,12 +186,30 @@ policySummary$state %<>%  str_replace('WASHINGTON DC', 'DISTRICT OF COLUMBIA') %
 policy %<>% group_by(RegionName) %>% filter(Date >= '2020-03-01' & Date <= endDate) %>% 
   mutate(med = median(GovernmentResponseIndex))
 
+# * Policy Plot ----
+tiff(filename = '../Figures/medianPlot.tiff', width = 1200, height = 1200)
 policy %>%
   filter(RegionName %in% c('Alabama', 'Ohio', 'New York', 'Texas', 
                            'South Dakota',  'California', 'Florida', 'North Carolina',
                            'Washington') ) %>% 
   ggplot(aes(x = Date, y = GovernmentResponseIndex, group = RegionName)) +
-  geom_line(size = 1.25) +
-  geom_hline(aes(yintercept = med), color = 'red') + 
-  facet_wrap(~RegionName) + theme_bw(base_size = 24) +
-  scale_x_date(date_breaks = "2 month", date_labels = "%b")
+  geom_line(size = 2) +
+  geom_hline(aes(yintercept = med), color = 'red', size = 1.25) + 
+  facet_wrap(~RegionName) + theme_bw(base_size = 30) +
+  scale_x_date(date_breaks = "2 month", date_labels = "%b") +
+  labs(y = 'Government Response Index', x = 'Month')
+invisible( dev.off() ) # to suppress the unwanted output from dev.off
+
+postscript(file = '../Figures/medianPlot.eps', width = 1200, height = 1200)
+policy %>%
+  filter(RegionName %in% c('Alabama', 'Ohio', 'New York', 'Texas', 
+                           'South Dakota',  'California', 'Florida', 'North Carolina',
+                           'Washington') ) %>% 
+  ggplot(aes(x = Date, y = GovernmentResponseIndex, group = RegionName)) +
+  geom_line(size = 2) +
+  geom_hline(aes(yintercept = med), color = 'red', size = 1.25) + 
+  facet_wrap(~RegionName) + theme_bw(base_size = 20) +
+  scale_x_date(date_breaks = "2 month", date_labels = "%b") +
+  labs(y = 'Government Response Index', x = 'Month')
+invisible( dev.off() ) # to suppress the unwanted output from dev.off
+
